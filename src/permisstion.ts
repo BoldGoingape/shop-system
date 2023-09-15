e3e971e5950f037544138a468f6eed2b81cacca9
wwj <304925106@aliyun.com>
2023-09-15 08:21:31 +0800

    完善路由鉴权

--- src/permisstion.ts
+++ src/permisstion.ts
@@ -1,11 +1,30 @@
 import { router } from './router/index'
 import nprogress from 'nprogress'
 import "nprogress/nprogress.css"
+import pinia  from './stores'
+import useUserStore from './stores/modules/user'
+import setting from './setting'
+const userStore= useUserStore(pinia)
+nprogress.configure({ showSpinner: false });
 // 路由鉴权
 // 全局前置守卫
 router.beforeEach((to: any, from: any, next: any) => {
+  document.title=`${setting.title}-${to.meta.title}`
   nprogress.start()
-  next()
+  const token = userStore.token;
+  if (token) {
+    if (to.path=='/login') {
+      next({path:'/'})
+    }else{
+      next()
+    }
+  }else{
+    if (to.path=='/login') {
+      next()
+    }else{
+      next({path:'/login',query:{redirect:to.path}})
+    }
+  }
 })
 // 后置全局守卫
 router.afterEach((to: any, from: any) => {
--- src/setting.ts
+++ src/setting.ts
@@ -1,6 +1,6 @@
 //项目logo标题配置
 export default {
-  title: '天津市气象局用户权限管理系统',
+  title: '权限管理系统',
   logo: '/public/header.png',
   // 是否显示logo
   logoHidden: true,
